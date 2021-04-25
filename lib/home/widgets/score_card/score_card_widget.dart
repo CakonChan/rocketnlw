207b269cc876046aeba7b858b88cb699cfd9670a
CakonChan <caioferrazporto@hotmail.com>
2021-04-24 21:02:33 -0300

    novas linhas para inserir animação

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -4,7 +4,8 @@ import 'package:rocketnlw/core/app_text_styles.dart';
 import 'package:rocketnlw/home/widgets/chart/chart_widget.dart';
 
 class ScoreCardWidget extends StatelessWidget {
-  const ScoreCardWidget({Key? key}) : super(key: key);
+  final double percent;
+  const ScoreCardWidget({Key? key, required this.percent}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,9 @@ class ScoreCardWidget extends StatelessWidget {
             children: [
               Expanded(
                 flex: 1,
-                child: ChartWidget(),
+                child: ChartWidget(
+                  percent: percent,
+                ),
               ),
               Expanded(
                 flex: 3,
